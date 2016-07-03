954f4e38a4bbc7e10ca0717b71130553072753c2
Chris Alley <chris@chrisalley.info>
2016-07-03 18:13:59 +1000

    Add page resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703074351) do
+ActiveRecord::Schema.define(version: 20160703081213) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
---
+++ spec/models/page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Page, type: :model do
+  describe 'fields' do
+    it { is_expected.to respond_to :title }
+    it { is_expected.to respond_to :slug }
+    it { is_expected.to respond_to :created_at }
+    it { is_expected.to respond_to :updated_at }
+  end
+end
