1e3b9136f3500d51e939897410eebd0db17af82e
Chris Alley <chris@chrisalley.info>
2016-07-03 18:00:55 +1000

    Describe fields in profile and user model specs

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
+  describe 'fields' do
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :email }
+    it { is_expected.to respond_to :created_at }
+    it { is_expected.to respond_to :updated_at }
+  end
+
   describe 'associations' do
     it { is_expected.to have_one :user }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'fields' do
+    it { is_expected.to respond_to :email }
+    it { is_expected.to respond_to :encrypted_password }
+    it { is_expected.to respond_to :reset_password_token }
+    it { is_expected.to respond_to :reset_password_sent_at }
+    it { is_expected.to respond_to :remember_created_at }
+    it { is_expected.to respond_to :sign_in_count }
+    it { is_expected.to respond_to :current_sign_in_at }
+    it { is_expected.to respond_to :last_sign_in_at }
+    it { is_expected.to respond_to :current_sign_in_ip }
+    it { is_expected.to respond_to :last_sign_in_ip }
+    it { is_expected.to respond_to :confirmation_token }
+    it { is_expected.to respond_to :confirmed_at }
+    it { is_expected.to respond_to :confirmation_sent_at }
+    it { is_expected.to respond_to :unconfirmed_email }
+    it { is_expected.to respond_to :failed_attempts }
+    it { is_expected.to respond_to :unlock_token }
+    it { is_expected.to respond_to :locked_at }
+    it { is_expected.to respond_to :administrator }
+    it { is_expected.to respond_to :profile_id }
+    it { is_expected.to respond_to :created_at }
+    it { is_expected.to respond_to :updated_at }
+  end
+
   describe 'associations' do
     it { is_expected.to belong_to :profile }
   end
