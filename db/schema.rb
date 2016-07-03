4bf7c9656af83202b52d3b68be115988ff7e0898
Chris Alley <chris@chrisalley.info>
2016-07-03 17:36:37 +1000

    Add profile resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703061947) do
+ActiveRecord::Schema.define(version: 20160703073540) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
