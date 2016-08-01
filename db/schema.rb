50f5cabbb63f9816b0100e7098896158b2b0b6ea
Chris Alley <chris@chrisalley.info>
2016-08-01 22:54:37 +1000

    Add administrator flag to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801115703) do
+ActiveRecord::Schema.define(version: 20160801125203) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
@@ -39,12 +39,13 @@ ActiveRecord::Schema.define(version: 20160801115703) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.string   "remember_token",     limit: 128,                 null: false
+    t.boolean  "administrator",                  default: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
